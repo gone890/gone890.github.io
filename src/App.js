8b9f43df11a2d621bb78ca3d556c9054739238cf
Evan <evan890kim@gmail.com>
2024-07-06 04:03:31 +1000

    app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1><code>gone890</code></h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          <code>Hello :{")"} </code>
+        </p>
+        <p><code>I'm Evan, here are my projects.</code></p>
       </header>
     </div>
   );
