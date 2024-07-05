f5cd3fe093050cf3e1c0b69b13a44bc980ee5993
Evan <evan890kim@gmail.com>
2024-07-06 03:52:29 +1000

    home changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useEffect } from 'react';
+
 import logo from './logo.png';
 import './App.css';
 
 function App() {
+
+  //set doc title to evan
+  function setDocTitle() {
+    useEffect(() => {
+      document.title = 'gone890 Projects';
+    }, []);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
