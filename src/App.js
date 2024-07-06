ceef0de7572ee544f9a1d6fa4d6640735ecfbe48
Evan <evan890kim@gmail.com>
2024-07-06 15:23:51 +1000

    added haha funny, remove later

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import React, { useState } from 'react';
 import logo from './logo.png';
+import calbean from './calbean_cover.png'
 import './App.css';
 
 function App() {
+
+  const [calvin, setCalvin] = useState(null)
+
+  function handleCalvin(e){
+    e.preventDefault();
+    setCalvin(1);
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <h1><code>gone890</code></h1>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {calvin ? <img src={calbean} className="App-logo" alt="logo" /> : null}
         <p>
           <code>Hello :{")"} </code>
         </p>
         <p><code>I'm Evan, here are my projects.</code></p>
+        <button onClick={handleCalvin}><code>calvin mode</code></button>
       </header>
     </div>
   );
