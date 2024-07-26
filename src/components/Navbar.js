22f96c07105e208b136f4b31ac950381ff3207e8
Evan <evan890kim@gmail.com>
2024-07-26 21:33:29 +1000

    create component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import style from '../style/Navbar.css';
+
+function Navbar() {
+
+  return (
+    <div className="navbar-outer"></div>
+  );
+}
+
+export default Navbar;
